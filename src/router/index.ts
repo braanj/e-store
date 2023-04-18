0a3638c8007e8aa8884f215c77433e57b69eb17b
Anjjar Brahim <anjar.bra@gmail.com>
2023-04-18 14:47:26 +0000

    Page de produit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
+import ProductView from '@/views/ProductView.vue'
 
 Vue.use(VueRouter)
 
@@ -13,10 +15,12 @@ const routes: Array<RouteConfig> = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutView
+  },
+  {
+    path: '/product',
+    name: 'product',
+    component: ProductView
   }
 ]
 
