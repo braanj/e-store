1e8f38d848884ff789198b06b271875537e95c0e
Anjjar Brahim <anjar.bra@gmail.com>
2023-04-27 19:40:53 +0100

    Update filter ui and add range filter

--- src/components/AccordionItem.vue
+++ src/components/AccordionItem.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="accordion-item">
     <span class="accordion-header" :id="'header_' + title">
-      <button class="accordion-button collapsed border-bottom" type="button" @click="toggled = !toggled">
+      <button class="accordion-button collapsed border-bottom" type="button">
         <span>{{ title }}</span>
       </button>
     </span>
 
     <transition apear enter-active-class="animate__slideInDown" leave-active-class="animate__slideOutUp">
-    <div class="animate__animated" :id="'tab_' + title" v-if="toggled">
-      <div class="accordion-body">
-        <slot></slot>
+      <div class="animate__animated" :id="'tab_' + title">
+        <div class="accordion-body">
+          <slot></slot>
+        </div>
       </div>
-    </div>
-  </transition>
+    </transition>
   </div>
 </template>
 
@@ -25,7 +25,7 @@ export default class AccordionItem extends Vue {
   @Prop() title!: string
   @Prop() index!: number
 
-  toggled = false
+  toggled = true
 }
 </script>
 
@@ -33,12 +33,13 @@ export default class AccordionItem extends Vue {
 .accordion-item {
   overflow: hidden;
   border: 0;
-  border-bottom: 2px solid var(--body-bg-color);
-  background-color: transparent;
+  width: 100%;
 
   .accordion-header {
     .accordion-button {
       text-transform: uppercase;
+      border-top-left-radius: 5px;
+      border-top-right-radius: 5px;
 
       &:focus {
         border-color: unset;
@@ -62,7 +63,8 @@ export default class AccordionItem extends Vue {
   }
 
   .accordion-body {
-    background-color: var(--element-bg-color);
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
   }
 }
 
